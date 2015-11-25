0e2cbd05d528bbd4cfd6d053809193c8d9411ebb
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-11-25 10:11:49 -0500

    finished form

--- views/admin/plugins/openseadragon-config-form.php
+++ views/admin/plugins/openseadragon-config-form.php
@@ -9,22 +9,6 @@
         array('checked' => (bool) get_option('openseadragon_embed_admin'))); ?>
     </div>
 </div>
-<div class="field">
-    <div id="openseadragon_width_admin_label" class="two columns alpha">
-        <label for="openseadragon_width_admin"><?php echo __('Viewer width, in pixels'); ?></label>
-    </div>
-    <div class="inputs five columns omega">
-        <?php echo $this->formText('openseadragon_width_admin', get_option('openseadragon_width_admin')); ?>
-    </div>
-</div>
-<div class="field">
-    <div id="openseadragon_height_admin_label" class="two columns alpha">
-        <label for="openseadragon_height_admin"><?php echo __('Viewer height, in pixels'); ?></label>
-    </div>
-    <div class="inputs five columns omega">
-        <?php echo $this->formText('openseadragon_height_admin', get_option('openseadragon_height_admin')); ?>
-    </div>
-</div>
 
 <h2>Public Theme</h2>
 
@@ -35,30 +19,5 @@
     <div class="inputs five columns omega">
         <?php echo $this->formCheckbox('openseadragon_embed_public', null, 
         array('checked' => (bool) get_option('openseadragon_embed_public'))); ?>
-    </div>
-</div>
-<div class="field">
-    <div id="openseadragon_css_override_public_label" class="two columns alpha">
-        <label for="openseadragon_css_override_public"><?php echo __('Override width/height in public item show pages using custom CSS?'); ?></label>
-    </div>
-    <div class="inputs five columns omega">
-        <?php echo $this->formCheckbox('openseadragon_css_override_public', null, 
-        array('checked' => (bool) get_option('openseadragon_css_override_public'))); ?>
-    </div>
-</div>
-<div class="field">
-    <div id="openseadragon_width_public_label" class="two columns alpha">
-        <label for="openseadragon_width_public"><?php echo __('Viewer width, in pixels'); ?></label>
-    </div>
-    <div class="inputs five columns omega">
-        <?php echo $this->formText('openseadragon_width_public', get_option('openseadragon_width_public')); ?>
-    </div>
-</div>
-<div class="field">
-    <div id="openseadragon_height_public_label" class="two columns alpha">
-        <label for="openseadragon_height_public"><?php echo __('Viewer height, in pixels'); ?></label>
-    </div>
-    <div class="inputs five columns omega">
-        <?php echo $this->formText('openseadragon_height_public', get_option('openseadragon_height_public')); ?>
     </div>
 </div>
