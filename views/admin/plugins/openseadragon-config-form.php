bb0327185054276d133b1ff3f05aad9fec7f9eb1
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-11-25 10:13:37 -0500

    form

--- views/admin/plugins/openseadragon-config-form.php
+++ views/admin/plugins/openseadragon-config-form.php
@@ -1,4 +1,4 @@
-<h2>Admin Theme</h2>
+<h2>Embed Options</h2>
 
 <div class="field">
     <div id="openseadragon_embed_admin_label" class="two columns alpha">
@@ -9,8 +9,6 @@
         array('checked' => (bool) get_option('openseadragon_embed_admin'))); ?>
     </div>
 </div>
-
-<h2>Public Theme</h2>
 
 <div class="field">
     <div id="openseadragon_embed_public_label" class="two columns alpha">
