1745a2464a16422af46748767ce1f9361cd64200
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-11-25 09:53:49 -0500

    some final changes to settings

--- views/shared/common/openseadragon.php
+++ views/shared/common/openseadragon.php
@@ -8,8 +8,10 @@
 					id: "osd-single",
 					prefixUrl: "<?=$button_path?>",
 					showNavigator: true,
+					navigatorSizeRatio: 0.1,
 					minZoomImageRatio: 0.8,
 					maxZoomPixelRatio: 1.5,
+					controlsFadeDelay: 1000,
 					tileSources: {
 						type: 'legacy-image-pyramid',
 						levels:<?php echo openseadragon_create_pyramid($images[0]); ?>
@@ -22,17 +24,19 @@
 	<div class="openseadragon-frame">
 		<div class="openseadragon" id="osd-sequence">
 			<script type="text/javascript">
+				OpenSeadragon.setString("Tooltips.NextPage", "Next");
+				OpenSeadragon.setString("Tooltips.PreviousPage","Previous");
 				OpenSeadragon({
 					id: "osd-sequence",
 					prefixUrl: "<?=$button_path?>",
 					showNavigator: true,
+					navigatorSizeRatio: 0.1,
 					minZoomImageRatio: 0.8,
 					maxZoomPixelRatio: 1.5,
+					controlsFadeDelay: 1000,
 					sequenceMode: true,
 					showReferenceStrip: true,
 					referenceStripSizeRatio: .1,
-					previousButton: 'Previous',
-					nextButton: 'Next',
 					tileSources: [
 						<?php foreach($images as $image): ?>
 						{type: 'legacy-image-pyramid',
