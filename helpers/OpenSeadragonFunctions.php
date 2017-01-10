f17ffc6865626568617addf5030fdcbcfeeb3213
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-01-10 10:50:48 -0500

    new fitdil url

--- helpers/OpenSeadragonFunctions.php
+++ helpers/OpenSeadragonFunctions.php
@@ -11,23 +11,23 @@ function openseadragon_create_mdid_pyramid($record_name, $record_id, $width, $he
 {
     $sizes = array('Medium', 'Large', 'Original');
     $pyramid = array();
-	$url = array('url' => 'https://fit.vrchost.com/media/get/' . $record_id . '/' . $record_name . '/');
+	$url = array('url' => 'https://fitdil.fitnyc.edu/media/get/' . $record_id . '/' . $record_name . '/');
 	$dimensions = array('height' => (int) $height, 'width' => (int) $width);
 	$pyramid[] = $url + $dimensions;
 	$height_1 = ceil(($height * 2) / 3);
 	$width_1 = ceil(($width * 2) / 3);
 	$dimensions_1 = array('height' => (int) $height_1, 'width' => (int) $width_1);
-	$url_1 = array('url' => 'https://fit.vrchost.com/media/get/' . $record_id . '/' . $record_name . '/' . $width_1 . 'x' . $height_1 . '/');
+	$url_1 = array('url' => 'https://fitdil.fitnyc.edu/media/get/' . $record_id . '/' . $record_name . '/' . $width_1 . 'x' . $height_1 . '/');
 	$pyramid[] = $url_1 + $dimensions_1;
 	$height_2 = ceil($height / 3);
 	$width_2 = ceil($width / 3);
 	$dimensions_2 = array('height' => (int) $height_2, 'width' => (int) $width_2);
-	$url_2 = array('url' => 'https://fit.vrchost.com/media/get/' . $record_id . '/' . $record_name . '/' . $width_2 . 'x' . $height_2 . '/');
+	$url_2 = array('url' => 'https://fitdil.fitnyc.edu/media/get/' . $record_id . '/' . $record_name . '/' . $width_2 . 'x' . $height_2 . '/');
 	$pyramid[] = $url_2 + $dimensions_2;
 	$height_3 = ceil($height / 6);
 	$width_3 = ceil($width / 6);
 	$dimensions_3 = array('height' => (int) $height_3, 'width' => (int) $width_3);
-	$url_3 = array('url' => 'https://fit.vrchost.com/media/get/' . $record_id . '/' . $record_name . '/' . $width_3 . 'x' . $height_3 . '/');
+	$url_3 = array('url' => 'https://fitdil.fitnyc.edu/media/get/' . $record_id . '/' . $record_name . '/' . $width_3 . 'x' . $height_3 . '/');
 	$pyramid[] = $url_3 + $dimensions_3;
     return json_encode($pyramid);
 }
