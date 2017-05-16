82cca99c204e3ae7e1009c9c6c9a930b9f660ce5
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-05-16 16:48:09 -0400

    testing

--- views/helpers/Openseadragon.php
+++ views/helpers/Openseadragon.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * OpenSeadragon (based on DPLA fork of the ZoomIt plugin)
- * 
+ *
  * @copyright Copyright 2014 Digital Public Library of America
  * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
@@ -12,12 +12,12 @@
  */
 class OpenSeadragon_View_Helper_Openseadragon extends Zend_View_Helper_Abstract
 {
-    protected $_supportedExtensions = array('bmp', 'gif', 'ico', 'jpeg', 'jpg', 
+    protected $_supportedExtensions = array('bmp', 'gif', 'ico', 'jpeg', 'jpg',
                                             'png', 'tiff', 'tif');
 
     /**
      * Return a OpenSeadragon image viewer for the provided files.
-     * 
+     *
      * @param File|array $files A File record or an array of File records.
      * @param int $width The width of the image viewer in pixels.
      * @param int $height The height of the image viewer in pixels.
@@ -31,7 +31,7 @@ class OpenSeadragon_View_Helper_Openseadragon extends Zend_View_Helper_Abstract
 
         // Return if there are no valid Flickr data.
         else {
-            return;
+            return '<p>This is completely broken</p>';
         }
 
         return $this->view->partial('common/openseadragon.php', array(
