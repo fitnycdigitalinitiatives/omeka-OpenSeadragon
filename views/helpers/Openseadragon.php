d13be5aeef8cf00d00fd0874859e8fbebe2d63ae
Joseph Anderson <fitnycdigitalinitiatives@users.noreply.github.com>
2017-06-27 14:47:50 -0400

    collection view

--- views/helpers/Openseadragon.php
+++ views/helpers/Openseadragon.php
@@ -33,7 +33,18 @@ class OpenSeadragon_View_Helper_Openseadragon extends Zend_View_Helper_Abstract
           ));
         }
         elseif (count($record_name) > 1) {
-          return openseadragon_create_mdid_collection($record_name, $record_id, $width, $height);
+          $tilesource = array();
+          $arrlength = count($record_id);
+          for ($x = 0; $x < $arrlength; $x++) {
+            if (($record_name[$x]) && ($record_id[$x]) && ($width[$x]) && ($height[$x])) {
+              $type = array('type' => 'legacy-image-pyramid');
+              $levels = array('levels' => openseadragon_create_mdid_pyramid($record_name[$x], $record_id[$x], $width[$x], $height[$x]));
+              $tilesource[] = $type + $levels;
+            }
+          }
+          return $this->view->partial('common/openseadragon_collection.php', array(
+              'tilesource' => json_encode($tilesource)
+          ));
         }
       }
       // Return if there are no valid Flickr data.
